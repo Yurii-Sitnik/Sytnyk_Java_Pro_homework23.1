8e17fea72b00c33d895ef506dbf9c01ccee165cd
Sytnyk Yurii <yoursit1980@gmail.com>
2023-03-11 19:06:55 +0200

    homework23.1

---
+++ src/main/java/org/example/homework23/mathOperations.java
@@ -0,0 +1,29 @@
+package org.example.homework23;
+
+public class mathOperations {
+
+    public static double addition(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtraction(double a, double b) {
+        return a - b;
+    }
+
+    public static double division(double a, double b) {
+        return a / b;
+    }
+
+    public static double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    public static double sqrt(double x) {
+        return Math.sqrt(x);
+    }
+
+    public static double round(double x) {
+        return Math.round(x);
+    }
+}
+
